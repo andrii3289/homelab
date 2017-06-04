3d60ef3a78a91d80db148550c09df31443801519
Your Name <you@example.com>
2017-06-04 13:55:49 +0000

    aligning

--- ip_finder.py
+++ ip_finder.py
@@ -8,9 +8,9 @@ for lease in conn.networkLookupByName('default').DHCPLeases():
 with open('/proc/net/arp') as f:
     for line in f.readlines():
         for k,v in vm.items():
-           if v == line.split()[3]:
-               if len(argv) > 1:
-                   if argv[1] == "-f" or "--full":
-                       print k,"has mac",v,"and","ip",line.split()[0]
-               else:
-                   print k,"has ip",line.split()[0]
+            if v == line.split()[3]:
+                if len(argv) > 1:
+                    if argv[1] == "-f" or "--full":
+                        print k,"has mac",v,"and","ip",line.split()[0]
+                else:
+                    print k,"has ip",line.split()[0]
